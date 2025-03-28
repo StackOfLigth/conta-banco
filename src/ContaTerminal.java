a767e29d621219fdb8ee3cd76313550904104621
GustavoFernandes <fernandes.man@outlook.com>
2025-03-27 22:04:58 -0300

    melhorias na mensagem final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -10,15 +10,15 @@ public class ContaTerminal {
         //DECLARANDO AS VARIAVEIS
         int numeroConta;
         String numeroAgencia;
-        String nomeClinte;
+        String nomeCliente;
         Double initDeposito;
-        Double initiSaldo = 0.0;
+        Double initSaldo = 0.0;
 
         //Entrada de dados
         System.out.println("Olá, Bem-Vindo Para criar sua conta, informe os seguintes dados: ");
 
         System.out.println("Digite seu nome: ");
-        nomeClinte = leitura.nextLine();
+        nomeCliente = leitura.nextLine();
 
         System.out.println("Digite o número da sua agencia: ");
         numeroAgencia = leitura.nextLine();
@@ -29,13 +29,15 @@ public class ContaTerminal {
         System.out.println("Digite o valor do depósito inicial: ");
         initDeposito = leitura.nextDouble();
 
-        Double saldoAtual = initDeposito + initiSaldo;
+        
+        Double saldoAtual = initDeposito + initSaldo;
 
-        String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque!", nomeClinte, numeroAgencia, numeroConta, saldoAtual);
+        //Formatação stiplo templateString
+        String mensagem = String.format("Olá %s,\nObrigado por criar uma conta em nosso banco, sua agência é: %s, conta: %d e seu saldo: R$ %.2f já está disponível para saque!", nomeCliente, numeroAgencia, numeroConta, saldoAtual);
 
         
-        //EXIBIR MENSAGENS
-        System.out.println(mensagem);
+        
+        System.out.println(mensagem);//EXIBIR MENSAGEM
         
         leitura.close(); // Fecha o Scanner após o uso
     }
